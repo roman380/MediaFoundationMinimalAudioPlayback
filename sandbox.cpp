8e4ef6b24363de7eeee4072a0259b4c36796ed3b
Roman Ryltsov <roman@alax.info>
2023-04-22 10:41:17 +0200

    Added wil and event name output

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,82 +1,229 @@
+#include <iterator>
+#include <utility>
 #include <iostream>
+
 #include <mfapi.h>
 #include <mfidl.h>
-#include <mfreadwrite.h>
-#include <wmcodecdsp.h>
-#include <combaseapi.h>
-
-#include <unknwn.h>
-#include <winrt/base.h>
 
 #pragma comment(lib, "mf.lib")
 #pragma comment(lib, "mfplat.lib")
 #pragma comment(lib, "mfuuid.lib")
-#pragma comment(lib, "mfreadwrite.lib")
+
+#include <unknwn.h>
+#include <winrt/base.h>
+
+#include <wil/resource.h>
+#include <wil/com.h>
 
 int main()
 {
     try
     {
         winrt::init_apartment();
-        winrt::check_hresult(MFStartup(MF_VERSION));
+        THROW_IF_FAILED(MFStartup(MF_VERSION));
 
-        winrt::com_ptr<IMFSourceResolver> SourceResolver;
-        winrt::check_hresult(MFCreateSourceResolver(SourceResolver.put()));
+        wil::com_ptr<IMFSourceResolver> SourceResolver;
+        THROW_IF_FAILED(MFCreateSourceResolver(SourceResolver.put()));
         MF_OBJECT_TYPE ObjectType = MF_OBJECT_INVALID;
-        winrt::com_ptr<IUnknown> MediaSourceUnknown;
-        winrt::check_hresult(SourceResolver->CreateObjectFromURL(L"..\\test.mp3", MF_RESOLUTION_MEDIASOURCE, nullptr, &ObjectType, MediaSourceUnknown.put()));
-        auto const MediaSource = MediaSourceUnknown.as<IMFMediaSource>();
+        wil::com_ptr<IUnknown> MediaSourceUnknown;
+        THROW_IF_FAILED(SourceResolver->CreateObjectFromURL(L"..\\test.mp3", MF_RESOLUTION_MEDIASOURCE, nullptr, &ObjectType, MediaSourceUnknown.put()));
+        auto const MediaSource = MediaSourceUnknown.query<IMFMediaSource>();
 
-        winrt::com_ptr<IMFTopology> Topology;
-        winrt::check_hresult(MFCreateTopology(Topology.put()));
+        wil::com_ptr<IMFTopology> Topology;
+        THROW_IF_FAILED(MFCreateTopology(Topology.put()));
 
-        winrt::com_ptr<IMFPresentationDescriptor> PresentationDescriptor;
-        winrt::check_hresult(MediaSource->CreatePresentationDescriptor(PresentationDescriptor.put()));
+        wil::com_ptr<IMFPresentationDescriptor> PresentationDescriptor;
+        THROW_IF_FAILED(MediaSource->CreatePresentationDescriptor(PresentationDescriptor.put()));
         BOOL Select;
-        winrt::com_ptr<IMFStreamDescriptor> StreamDescriptor;
-        winrt::check_hresult(PresentationDescriptor->GetStreamDescriptorByIndex(0, &Select, StreamDescriptor.put()));
+        wil::com_ptr<IMFStreamDescriptor> StreamDescriptor;
+        THROW_IF_FAILED(PresentationDescriptor->GetStreamDescriptorByIndex(0, &Select, StreamDescriptor.put()));
         WINRT_ASSERT(Select && StreamDescriptor);
-        winrt::com_ptr<IMFTopologyNode> SourceNode;
-        winrt::check_hresult(MFCreateTopologyNode(MF_TOPOLOGY_SOURCESTREAM_NODE, SourceNode.put()));
-        winrt::check_hresult(SourceNode->SetUnknown(MF_TOPONODE_SOURCE, MediaSource.get()));
-        winrt::check_hresult(SourceNode->SetUnknown(MF_TOPONODE_PRESENTATION_DESCRIPTOR, PresentationDescriptor.get()));
-        winrt::check_hresult(SourceNode->SetUnknown(MF_TOPONODE_STREAM_DESCRIPTOR, StreamDescriptor.get()));
-        winrt::check_hresult(Topology->AddNode(SourceNode.get()));
+        wil::com_ptr<IMFTopologyNode> SourceNode;
+        THROW_IF_FAILED(MFCreateTopologyNode(MF_TOPOLOGY_SOURCESTREAM_NODE, SourceNode.put()));
+        THROW_IF_FAILED(SourceNode->SetUnknown(MF_TOPONODE_SOURCE, MediaSource.get()));
+        THROW_IF_FAILED(SourceNode->SetUnknown(MF_TOPONODE_PRESENTATION_DESCRIPTOR, PresentationDescriptor.get()));
+        THROW_IF_FAILED(SourceNode->SetUnknown(MF_TOPONODE_STREAM_DESCRIPTOR, StreamDescriptor.get()));
+        THROW_IF_FAILED(Topology->AddNode(SourceNode.get()));
 
-        winrt::com_ptr<IMFActivate> Activate;
-        winrt::check_hresult(MFCreateAudioRendererActivate(Activate.put()));
-        winrt::com_ptr<IMFTopologyNode> OutputNode;
-        winrt::check_hresult(MFCreateTopologyNode(MF_TOPOLOGY_OUTPUT_NODE, OutputNode.put())); // https://learn.microsoft.com/en-us/windows/win32/medfound/creating-output-nodes#creating-an-output-node-from-an-activation-object
-        winrt::check_hresult(OutputNode->SetObject(Activate.get()));
-        winrt::check_hresult(Topology->AddNode(OutputNode.get()));
+        wil::com_ptr<IMFActivate> Activate;
+        THROW_IF_FAILED(MFCreateAudioRendererActivate(Activate.put()));
+        wil::com_ptr<IMFTopologyNode> OutputNode;
+        THROW_IF_FAILED(MFCreateTopologyNode(MF_TOPOLOGY_OUTPUT_NODE, OutputNode.put())); // https://learn.microsoft.com/en-us/windows/win32/medfound/creating-output-nodes#creating-an-output-node-from-an-activation-object
+        THROW_IF_FAILED(OutputNode->SetObject(Activate.get()));
+        THROW_IF_FAILED(Topology->AddNode(OutputNode.get()));
 
-        winrt::check_hresult(SourceNode->ConnectOutput(0, OutputNode.get(), 0));
+        THROW_IF_FAILED(SourceNode->ConnectOutput(0, OutputNode.get(), 0));
 
-        winrt::com_ptr<IMFMediaSession> MediaSession;
-        winrt::check_hresult(MFCreateMediaSession(nullptr, MediaSession.put()));
-        winrt::check_hresult(MediaSession->SetTopology(0, Topology.get()));
+        wil::com_ptr<IMFMediaSession> MediaSession;
+        THROW_IF_FAILED(MFCreateMediaSession(nullptr, MediaSession.put()));
+        THROW_IF_FAILED(MediaSession->SetTopology(0, Topology.get()));
 
         PROPVARIANT StartTime;
         PropVariantInit(&StartTime);
         StartTime.vt = VT_I8;
         StartTime.hVal.QuadPart = 0;
-        winrt::check_hresult(MediaSession->Start(nullptr, &StartTime));
+        THROW_IF_FAILED(MediaSession->Start(nullptr, &StartTime));
 
-        auto const MediaEventGenerator = MediaSession.as<IMFMediaEventGenerator>();
+        auto const MediaEventGenerator = MediaSession.query<IMFMediaEventGenerator>();
         for(; ; )
         {
-            winrt::com_ptr<IMFMediaEvent> MediaEvent;
-            winrt::check_hresult(MediaEventGenerator->GetEvent(0, MediaEvent.put()));
+            wil::com_ptr<IMFMediaEvent> MediaEvent;
+            THROW_IF_FAILED(MediaEventGenerator->GetEvent(0, MediaEvent.put()));
             MediaEventType Type;
-            winrt::check_hresult(MediaEvent->GetType(&Type));
-            std::wcout << L"Event: " << static_cast<int>(Type) << std::endl;
+            THROW_IF_FAILED(MediaEvent->GetType(&Type));
+            
+            #pragma region MediaEventType Names
+            static std::pair<MediaEventType, char const*> constexpr const g_Items[]
+            {
+                #define A(Identifier) std::make_pair(Identifier, #Identifier),
+                A(MEUnknown)
+                A(MEError)
+                A(MEExtendedType)
+                A(MENonFatalError)
+                A(MEGenericV1Anchor)
+                A(MESessionUnknown)
+                A(MESessionTopologySet)
+                A(MESessionTopologiesCleared)
+                A(MESessionStarted)
+                A(MESessionPaused)
+                A(MESessionStopped)
+                A(MESessionClosed)
+                A(MESessionEnded)
+                A(MESessionRateChanged)
+                A(MESessionScrubSampleComplete)
+                A(MESessionCapabilitiesChanged)
+                A(MESessionTopologyStatus)
+                A(MESessionNotifyPresentationTime)
+                A(MENewPresentation)
+                A(MELicenseAcquisitionStart)
+                A(MELicenseAcquisitionCompleted)
+                A(MEIndividualizationStart)
+                A(MEIndividualizationCompleted)
+                A(MEEnablerProgress)
+                A(MEEnablerCompleted)
+                A(MEPolicyError)
+                A(MEPolicyReport)
+                A(MEBufferingStarted)
+                A(MEBufferingStopped)
+                A(MEConnectStart)
+                A(MEConnectEnd)
+                A(MEReconnectStart)
+                A(MEReconnectEnd)
+                A(MERendererEvent)
+                A(MESessionStreamSinkFormatChanged)
+                A(MESessionV1Anchor)
+                A(MESourceUnknown)
+                A(MESourceStarted)
+                A(MEStreamStarted)
+                A(MESourceSeeked)
+                A(MEStreamSeeked)
+                A(MENewStream)
+                A(MEUpdatedStream)
+                A(MESourceStopped)
+                A(MEStreamStopped)
+                A(MESourcePaused)
+                A(MEStreamPaused)
+                A(MEEndOfPresentation)
+                A(MEEndOfStream)
+                A(MEMediaSample)
+                A(MEStreamTick)
+                A(MEStreamThinMode)
+                A(MEStreamFormatChanged)
+                A(MESourceRateChanged)
+                A(MEEndOfPresentationSegment)
+                A(MESourceCharacteristicsChanged)
+                A(MESourceRateChangeRequested)
+                A(MESourceMetadataChanged)
+                A(MESequencerSourceTopologyUpdated)
+                A(MESourceV1Anchor)
+                A(MESinkUnknown)
+                A(MEStreamSinkStarted)
+                A(MEStreamSinkStopped)
+                A(MEStreamSinkPaused)
+                A(MEStreamSinkRateChanged)
+                A(MEStreamSinkRequestSample)
+                A(MEStreamSinkMarker)
+                A(MEStreamSinkPrerolled)
+                A(MEStreamSinkScrubSampleComplete)
+                A(MEStreamSinkFormatChanged)
+                A(MEStreamSinkDeviceChanged)
+                A(MEQualityNotify)
+                A(MESinkInvalidated)
+                A(MEAudioSessionNameChanged)
+                A(MEAudioSessionVolumeChanged)
+                A(MEAudioSessionDeviceRemoved)
+                A(MEAudioSessionServerShutdown)
+                A(MEAudioSessionGroupingParamChanged)
+                A(MEAudioSessionIconChanged)
+                A(MEAudioSessionFormatChanged)
+                A(MEAudioSessionDisconnected)
+                A(MEAudioSessionExclusiveModeOverride)
+                A(MESinkV1Anchor)
+                A(MECaptureAudioSessionVolumeChanged)
+                A(MECaptureAudioSessionDeviceRemoved)
+                A(MECaptureAudioSessionFormatChanged)
+                A(MECaptureAudioSessionDisconnected)
+                A(MECaptureAudioSessionExclusiveModeOverride)
+                A(MECaptureAudioSessionServerShutdown)
+                A(MESinkV2Anchor)
+                A(METrustUnknown)
+                A(MEPolicyChanged)
+                A(MEContentProtectionMessage)
+                A(MEPolicySet)
+                A(METrustV1Anchor)
+                A(MEWMDRMLicenseBackupCompleted)
+                A(MEWMDRMLicenseBackupProgress)
+                A(MEWMDRMLicenseRestoreCompleted)
+                A(MEWMDRMLicenseRestoreProgress)
+                A(MEWMDRMLicenseAcquisitionCompleted)
+                A(MEWMDRMIndividualizationCompleted)
+                A(MEWMDRMIndividualizationProgress)
+                A(MEWMDRMProximityCompleted)
+                A(MEWMDRMLicenseStoreCleaned)
+                A(MEWMDRMRevocationDownloadCompleted)
+                A(MEWMDRMV1Anchor)
+                A(METransformUnknown)
+                A(METransformNeedInput)
+                A(METransformHaveOutput)
+                A(METransformDrainComplete)
+                A(METransformMarker)
+                A(METransformInputStreamStateChanged)
+                A(MEByteStreamCharacteristicsChanged)
+                A(MEVideoCaptureDeviceRemoved)
+                A(MEVideoCaptureDevicePreempted)
+                A(MEStreamSinkFormatInvalidated)
+                A(MEEncodingParameters)
+                A(MEContentProtectionMetadata)
+                A(MEDeviceThermalStateChanged)
+                //A(MEReservedMax)
+                #undef A
+            };
+            #pragma endregion
+            auto const Iterator = std::find_if(std::cbegin(g_Items), std::cend(g_Items), [=] (auto&& Item) { return Item.first == Type; });
+            if(Iterator != std::cend(g_Items))
+                std::wcout << L"Event: " << Iterator->second << std::endl;
+            else
+                std::wcout << L"Event: " << static_cast<int>(Type) << std::endl;
         }
 
-        winrt::check_hresult(MFShutdown());
+        /*
+            Event: MESessionTopologySet
+            Event: MESessionNotifyPresentationTime
+            Event: MESessionCapabilitiesChanged
+            Event: MESessionTopologyStatus
+            Event: MESessionTopologyStatus
+            Event: MEExtendedType
+            Event: MESessionCapabilitiesChanged
+            Event: MESessionStarted
+            Event: MEEndOfPresentation
+            ...
+            Event: MESessionTopologyStatus
+            Event: MESessionCapabilitiesChanged
+            Event: MESessionEnded
+        */ 
+
+        THROW_IF_FAILED(MFShutdown());
     }
-    catch(winrt::hresult_error const& Exception)
-    {
-        std::wcerr << L"Exception: " << Exception.message().c_str() << std::endl;
-    }
+    CATCH_LOG();
     return 0;
 }
